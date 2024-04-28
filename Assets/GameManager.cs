3ef055fafbd0ec93c6bc39ee22f8128725370b9e
k-mitani <k-mitani@unique21.co.jp>
2024-04-29 01:18:52 +0900

    勝ち負け処理

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,6 +8,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject suki;
     [SerializeField] private SoundManager sounds;
 
+    [SerializeField] private Player player1;
+    [SerializeField] private Player player2;
+
+    [SerializeField] private bool isInSuki = false;
+
     void Start()
     {
         suki.SetActive(false);
@@ -18,6 +23,7 @@ public class GameManager : MonoBehaviour
     {
         var wait = Random.value * waitShowSukiDurationMax;
         yield return new WaitForSeconds(wait);
+        isInSuki = true;
         suki.SetActive(true);
         sounds.PlaySeShowSuki();
     }
@@ -26,6 +32,32 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (isInSuki)
+        {
+            var p1pushed = Input.GetKey(KeyCode.A);
+            var p2pushed = Input.GetKey(KeyCode.L);
+            // p1の勝ち
+            if (p1pushed && !p2pushed)
+            {
+                isInSuki = false;
+                player1.OnWin();
+                player2.OnLose();
+            }
+            // p2の勝ち
+            else if (!p1pushed && p2pushed)
+            {
+                isInSuki = false;
+                player1.OnLose();
+                player2.OnWin();
+            }
+            // 同着、仕切り直し
+            else if (p1pushed && p2pushed)
+            {
+                isInSuki = false;
+                player1.OnKamae();
+                player2.OnKamae();
+                StartCoroutine(ShowSuki());
+            }
+        }
     }
 }
---
+++ Assets/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Sprite spriteNoutou;
+    [SerializeField] private Sprite spriteKamae;
+    [SerializeField] private Sprite spriteKiru;
+    [SerializeField] private Sprite spriteLoseBody;
+
+    [SerializeField] private Rigidbody2D loseHead;
+
+    private new SpriteRenderer renderer;
+
+    private void Awake()
+    {
+        TryGetComponent(out renderer);
+        renderer.sprite = spriteNoutou;
+        loseHead.gameObject.SetActive(false);
+    }
+
+    public void OnKamae()
+    {
+        renderer.sprite = spriteKamae;
+    }
+
+    public void OnWin()
+    {
+        renderer.sprite = spriteKiru;
+    }
+
+    public void OnLose()
+    {
+        renderer.sprite = spriteLoseBody;
+        loseHead.gameObject.SetActive(true);
+    }
+
+}
